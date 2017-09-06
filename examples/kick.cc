1ab9eb86830c92845671c5f5cbcd8cc75506cbe3
Elias Aebi <eyelash@users.noreply.github.com>
2017-09-06 11:36:53 +0200

    add a second kick

--- examples/kick.cc
+++ examples/kick.cc
@@ -14,6 +14,19 @@ public:
 	}
 };
 
+class Kick2: public Node<Sample> {
+	Osc osc;
+	Automation frequency;
+	Automation envelope;
+public:
+	Kick2(): frequency("3000 3000/.0005 500/.002 150/.01 50/.1"), envelope("0 .8/.0002 .8/.2 0/.1") {
+		frequency >> osc.frequency;
+	}
+	Sample produce() override {
+		return get(osc) * get(envelope);
+	}
+};
+
 int main() {
 	Kick kick;
 	WAVOutput wav("kick.wav");
